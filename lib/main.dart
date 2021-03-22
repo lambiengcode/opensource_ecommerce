38a6ff4c58de2e90d97ba3bab0f7be72153c4afb
Dao Hong Vinh <60530946+lambiengcode@users.noreply.github.com>
2021-03-22 17:13:29 +0700

    enable log

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:delivery_hub/src/app.dart';
 import 'package:delivery_hub/src/lang/translation_service.dart';
+import 'package:delivery_hub/src/shared/logger_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() {
   runApp(GetMaterialApp(
     title: 'Whoru',
-    //showPerformanceOverlay: true,
+    // showPerformanceOverlay: true,
     debugShowCheckedModeBanner: false,
     initialRoute: '/root',
     defaultTransition: Transition.native,
     locale: TranslationService.locale,
     fallbackLocale: TranslationService.fallbackLocale,
     translations: TranslationService(),
+    enableLog: true,
+    logWriterCallback: Logger.write,
     getPages: [
       GetPage(
         name: '/root',
