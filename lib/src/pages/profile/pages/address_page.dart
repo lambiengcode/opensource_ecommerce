5b3b899942b71dbc039d0e374549aaf83fa30c59
hongvinhmobile <lambiengcode@gmail.com>
2021-04-27 07:46:56 +0700

    [controller] done calculator distances

--- lib/src/pages/profile/pages/address_page.dart
+++ lib/src/pages/profile/pages/address_page.dart
@@ -8,6 +8,7 @@ import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_maps_place_picker/google_maps_place_picker.dart';
 import 'package:location/location.dart';
+import 'dart:math';
 
 class AddressPage extends StatefulWidget {
   const AddressPage({Key key}) : super(key: key);
@@ -23,6 +24,27 @@ class _AddressPageState extends State<AddressPage> {
   PickResult selectedPlace;
   LocationData currentLocation;
   Future<dynamic> _myLocation;
+  double calculateDistance(lat1, lon1, lat2, lon2) {
+    var p = 0.017453292519943295;
+    var c = cos;
+    var a = 0.5 -
+        c((lat2 - lat1) * p) / 2 +
+        c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p)) / 2;
+    return 12742 * asin(sqrt(a));
+  }
+
+  List<dynamic> data = [
+    {"lat": 44.968046, "lng": -94.420307},
+    {"lat": 44.33328, "lng": -89.132008},
+    {"lat": 33.755787, "lng": -116.359998},
+    {"lat": 33.844843, "lng": -116.54911},
+    {"lat": 44.92057, "lng": -93.44786},
+    {"lat": 44.240309, "lng": -91.493619},
+    {"lat": 44.968041, "lng": -94.419696},
+    {"lat": 44.333304, "lng": -89.132027},
+    {"lat": 33.755783, "lng": -116.360066},
+    {"lat": 33.844847, "lng": -116.549069},
+  ];
 
   getUserLocation() async {
     //call this async method from whereever you need
@@ -170,6 +192,12 @@ class _AddressPageState extends State<AddressPage> {
     super.initState();
     _myLocation = getUserLocation();
     //controller.streamProfile();
+    double totalDistance = 0;
+    for (var i = 0; i < data.length - 1; i++) {
+      totalDistance = calculateDistance(data[i]["lat"], data[i]["lng"],
+          data[i + 1]["lat"], data[i + 1]["lng"]);
+      print(totalDistance);
+    }
   }
 
   @override
