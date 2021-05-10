30b2a1fa5313bff84030b66bf5484f4ac7384318
Dao Hong Vinh <60530946+lambiengcode@users.noreply.github.com>
2021-05-10 19:27:17 +0700

    [config] done config firebase for android

--- lib/src/pages/transport/widgets/bottom_sheet_set_price.dart
+++ lib/src/pages/transport/widgets/bottom_sheet_set_price.dart
@@ -29,247 +29,199 @@ class _BottomSetPriceState extends State<BottomSetPrice> {
           top: Radius.circular(20.0),
         ),
       ),
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(height: 12.5),
-            Container(
-              height: 4.0,
-              margin: EdgeInsets.symmetric(horizontal: _size.width * .35),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30.0),
-                color: mCD,
-                boxShadow: [
-                  BoxShadow(
-                    color: mCD,
-                    offset: Offset(2.0, 2.0),
-                    blurRadius: 2.0,
-                  ),
-                  BoxShadow(
-                    color: mCL,
-                    offset: Offset(-1.0, -1.0),
-                    blurRadius: 1.0,
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  widget.title + '\tService',
-                  style: TextStyle(
-                    color: colorDarkGrey,
-                    fontFamily: 'Lato',
-                    fontSize: width / 22.5,
-                    fontWeight: FontWeight.w600,
-                  ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(height: 12.5),
+          Container(
+            height: 4.0,
+            margin: EdgeInsets.symmetric(horizontal: _size.width * .35),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30.0),
+              color: mCD,
+              boxShadow: [
+                BoxShadow(
+                  color: mCD,
+                  offset: Offset(2.0, 2.0),
+                  blurRadius: 2.0,
                 ),
-                GestureDetector(
-                  onTap: () => Get.back(),
-                  child: Text(
-                    'Save',
-                    style: TextStyle(
-                      color: colorPrimary,
-                      fontFamily: 'Lato',
-                      fontSize: width / 22.5,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
+                BoxShadow(
+                  color: mCL,
+                  offset: Offset(-1.0, -1.0),
+                  blurRadius: 1.0,
                 ),
               ],
             ),
-            SizedBox(height: 15.0),
-            Divider(
-              thickness: .15,
-              height: .15,
-              color: fCL,
-            ),
-            SizedBox(height: 15.0),
-            Text(
-              '• Setting Price',
-              style: TextStyle(
-                color: colorPrimary,
-                fontFamily: 'Lato',
-                fontSize: width / 28.5,
-                fontWeight: FontWeight.w400,
+          ),
+          SizedBox(height: 20.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                widget.title + '\tService',
+                style: TextStyle(
+                  color: colorDarkGrey,
+                  fontFamily: 'Lato',
+                  fontSize: width / 22.5,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
+              GestureDetector(
+                onTap: () => Get.back(),
+                child: Text(
+                  'Save',
+                  style: TextStyle(
+                    color: colorPrimary,
+                    fontFamily: 'Lato',
+                    fontSize: width / 22.5,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 15.0),
+          Divider(
+            thickness: .15,
+            height: .15,
+            color: fCL,
+          ),
+          SizedBox(height: 15.0),
+          Text(
+            '• Setting Price',
+            style: TextStyle(
+              color: colorPrimary,
+              fontFamily: 'Lato',
+              fontSize: width / 28.5,
+              fontWeight: FontWeight.w400,
             ),
-            Container(
-              height: width * .18,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 12.0,
+          ),
+          SizedBox(height: 4.0),
+          Container(
+            height: width * .19,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 8.0,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        8.0,
                       ),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(
-                          8.0,
+                      color: mC,
+                      boxShadow: [
+                        BoxShadow(
+                          color: mCD,
+                          offset: Offset(2, 2),
+                          blurRadius: 2,
                         ),
-                        color: mC,
-                        boxShadow: [
-                          BoxShadow(
-                            color: mCD,
-                            offset: Offset(2, 2),
-                            blurRadius: 2,
-                          ),
-                          BoxShadow(
-                            color: mCL,
-                            offset: Offset(-2, -2),
-                            blurRadius: 2,
-                          ),
-                        ],
+                        BoxShadow(
+                          color: mCL,
+                          offset: Offset(-2, -2),
+                          blurRadius: 2,
+                        ),
+                      ],
+                    ),
+                    alignment: Alignment.center,
+                    child: TextFormField(
+                      focusNode: _focusNode,
+                      controller: _msgController,
+                      onFieldSubmitted: (val) => null,
+                      cursorColor: fCL,
+                      cursorRadius: Radius.circular(30.0),
+                      keyboardType: TextInputType.text,
+                      style: TextStyle(
+                        color: colorTitle,
+                        fontSize: width / 26.0,
+                        fontWeight: FontWeight.w400,
                       ),
-                      alignment: Alignment.center,
-                      child: TextFormField(
-                        autofocus: true,
-                        focusNode: _focusNode,
-                        controller: _msgController,
-                        onFieldSubmitted: (val) => null,
-                        cursorColor: fCL,
-                        cursorRadius: Radius.circular(30.0),
-                        keyboardType: TextInputType.text,
-                        style: TextStyle(
-                          color: colorTitle,
+                      onChanged: (val) {},
+                      textAlign: TextAlign.start,
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.only(
+                          top: 16.0,
+                          left: 16.0,
+                        ),
+                        border: InputBorder.none,
+                        hintText: "Type price...",
+                        hintStyle: TextStyle(
+                          color: fCL,
                           fontSize: width / 26.0,
                           fontWeight: FontWeight.w400,
                         ),
-                        onChanged: (val) {},
-                        textAlign: TextAlign.start,
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.only(
-                            top: 14.0,
-                            left: 24.0,
-                          ),
-                          border: InputBorder.none,
-                          hintText: "Type price...",
-                          hintStyle: TextStyle(
-                            color: fCL,
-                            fontSize: 15.0,
-                            fontWeight: FontWeight.w400,
-                          ),
-                          suffixIcon: Padding(
-                            padding: EdgeInsets.only(top: 16.0, right: 8.0),
-                            child: Text(
-                              price,
-                              style: TextStyle(
-                                color: colorDarkGrey,
-                                fontSize: width / 30.0,
-                                fontWeight: FontWeight.w400,
-                              ),
+                        suffixIcon: Padding(
+                          padding: EdgeInsets.only(top: 16.0, right: 8.0),
+                          child: Text(
+                            price,
+                            style: TextStyle(
+                              color: fCL,
+                              fontSize: width / 31.5,
+                              fontWeight: FontWeight.w400,
                             ),
                           ),
                         ),
-                        inputFormatters: [
-                          FilteringTextInputFormatter.digitsOnly,
-                          TextInputFormatter.withFunction((oldValue, newValue) {
-                            if (newValue.text.isEmpty) {
-                              return newValue.copyWith(text: '');
-                            } else if (newValue.text.compareTo(oldValue.text) !=
-                                0) {
-                              final int selectionIndexFromTheRight =
-                                  newValue.text.length - newValue.selection.end;
-                              final f = NumberFormat("#,###");
-                              final number = int.parse(newValue.text
-                                  .replaceAll(f.symbols.GROUP_SEP, ''));
-                              final newString = f.format(number);
-                              return TextEditingValue(
-                                text: newString,
-                                selection: TextSelection.collapsed(
-                                    offset: newString.length -
-                                        selectionIndexFromTheRight),
-                              );
-                            } else {
-                              return newValue;
-                            }
-                          })
-                        ],
                       ),
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly,
+                        TextInputFormatter.withFunction((oldValue, newValue) {
+                          if (newValue.text.isEmpty) {
+                            return newValue.copyWith(text: '');
+                          } else if (newValue.text.compareTo(oldValue.text) !=
+                              0) {
+                            final int selectionIndexFromTheRight =
+                                newValue.text.length - newValue.selection.end;
+                            final f = NumberFormat("#,###");
+                            final number = int.parse(newValue.text
+                                .replaceAll(f.symbols.GROUP_SEP, ''));
+                            final newString = f.format(number);
+                            return TextEditingValue(
+                              text: newString,
+                              selection: TextSelection.collapsed(
+                                  offset: newString.length -
+                                      selectionIndexFromTheRight),
+                            );
+                          } else {
+                            return newValue;
+                          }
+                        })
+                      ],
                     ),
                   ),
-                ],
-              ),
-            ),
-            SizedBox(height: 2.5),
-            Row(
-              children: [
-                _buildButtonSwitch('VNĐ / Km', price == 'VNĐ / Km'),
-                SizedBox(width: 16.0),
-                _buildButtonSwitch('VNĐ / Kg', price == 'VNĐ / Kg'),
+                ),
               ],
             ),
-            SizedBox(height: 16.0),
-            Text(
-              '• Available',
-              style: TextStyle(
-                color: colorPrimary,
-                fontFamily: 'Lato',
-                fontSize: width / 28.5,
-                fontWeight: FontWeight.w400,
-              ),
+          ),
+          SizedBox(height: 2.5),
+          Row(
+            children: [
+              _buildButtonSwitch('VNĐ / Km', price == 'VNĐ / Km'),
+              SizedBox(width: 16.0),
+              _buildButtonSwitch('VNĐ / Kg', price == 'VNĐ / Kg'),
+            ],
+          ),
+          SizedBox(height: 16.0),
+          Text(
+            '• Available',
+            style: TextStyle(
+              color: colorPrimary,
+              fontFamily: 'Lato',
+              fontSize: width / 28.5,
+              fontWeight: FontWeight.w400,
             ),
-            SizedBox(height: 12.0),
-            Row(
-              children: [
-                _buildButtonSwitch('On', available == 'On'),
-                SizedBox(width: 16.0),
-                _buildButtonSwitch('Off', available == 'Off'),
-              ],
-            ),
-            SizedBox(height: 20.0),
-            Divider(
-              thickness: .15,
-              height: .15,
-              color: fCL,
-            ),
-            // Divider(
-            //   thickness: .2,
-            //   height: .2,
-            //   color: colorDarkGrey,
-            //   endIndent: 6.0,
-            //   indent: 6.0,
-            // ),
-            // SizedBox(height: 4.0),
-            // NeumorphicButton(
-            //   onPressed: () => Get.back(),
-            //   duration: Duration(milliseconds: 200),
-            //   style: NeumorphicStyle(
-            //     shape: NeumorphicShape.convex,
-            //     boxShape: NeumorphicBoxShape.roundRect(
-            //       BorderRadius.circular(30.0),
-            //     ),
-            //     depth: 10.0,
-            //     intensity: .8,
-            //     color: colorPrimary.withOpacity(.85),
-            //   ),
-            //   margin: EdgeInsets.symmetric(horizontal: 6.0),
-            //   padding: EdgeInsets.symmetric(
-            //     vertical: 16.0,
-            //     horizontal: 28.0,
-            //   ),
-            //   child: Row(
-            //     mainAxisAlignment: MainAxisAlignment.center,
-            //     children: [
-            //       Text(
-            //         'confirm'.trArgs(),
-            //         style: TextStyle(
-            //           color: mC,
-            //           fontSize: width / 26.5,
-            //           fontFamily: 'Lato',
-            //           fontWeight: FontWeight.w600,
-            //         ),
-            //       ),
-            //     ],
-            //   ),
-            // ),
-            SizedBox(height: 32.0),
-          ],
-        ),
+          ),
+          SizedBox(height: 12.0),
+          Row(
+            children: [
+              _buildButtonSwitch('On', available == 'On'),
+              SizedBox(width: 16.0),
+              _buildButtonSwitch('Off', available == 'Off'),
+            ],
+          ),
+          SizedBox(height: 32.0),
+        ],
       ),
     );
   }
